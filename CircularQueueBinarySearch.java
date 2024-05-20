5b0390dec133b08799344eb27da8cfe04eb3b44c
Aj0204 <167733643+Aj0204@users.noreply.github.com>
2024-05-20 19:42:58 +0530

    Add files via upload

---
+++ CircularQueueBinarySearch.java
@@ -0,0 +1,45 @@
+package wiprotraining;
+public class CircularQueueBinarySearch {
+
+    public static int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] <= nums[high]) {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            } else {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+
+        int index = search(nums, target);
+        if (index != -1) {
+            System.out.println("Element " + target + " found at index " + index);
+        } else {
+            System.out.println("Element " + target + " not found");
+        }
+    }
+}
+
